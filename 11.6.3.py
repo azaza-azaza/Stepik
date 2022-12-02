d16cef3f8f1dbc5b0a84b05692b49cb4b0fc0af8
azaza-azaza <stoyanov.a.s@yandex.ru>
2022-12-02 15:52:59 +0300

    first

---
+++ 11.6.3.py
@@ -0,0 +1,15 @@
+num = input()
+
+start = num[0]
+num = int(num[1:])
+answer = list()
+
+for el in range(num):
+    request = input()
+    if '#' in request:
+        ind = request.index('#')
+        answer.append(request[:ind].rstrip())
+    else:
+        answer.append(request)
+
+print(*answer, sep = '\n')
