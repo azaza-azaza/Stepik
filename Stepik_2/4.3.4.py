39348ba4c4d48cc77de82084660257a5617e5c23
azaza-azaza <stoyanov.a.s@yandex.ru>
2023-05-11 11:00:04 +0300

    first

--- Stepik_2/4.3.4.py
+++ Stepik_2/4.3.4.py
@@ -1,2 +1,10 @@
 n = int(input())
 
+def pascal(num):
+    row =[1]
+    print(*row)
+    for i in range(1, num):
+        row_1 = [1]
+        for j in range(len(row)-1):
+            row_1.append()
+11
